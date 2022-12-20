ba28aef5eb161fb61269b723725d8072f54a78c4
joshnaganja <119840321+joshnaganja@users.noreply.github.com>
2022-12-20 21:03:59 +0530

    #include<stdio.h> int main() {     int n,a,i,j,c;     scanf("%d%d",&n,&a);     for(i=1;i<=n;i++)     {         if(i%2!==0)         {             c=a*i;             printf("%d x %d = %d ",a,i,c);         }
        }
    }

---
+++ Tables.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,i,j,c;
+    scanf("%d%d",&n,&a);
+    for(i=1;i<=n;i++)
+    {
+        if(i%2!==0)
+        {
+            c=a*i;
+            printf("%d x %d = %d
+",a,i,c);
+        }
+        
+    }
+}
