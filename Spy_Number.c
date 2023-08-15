def83e24b10e3888ae9e5dcbc6fe41e0e83acc1c
joshnaganja <119840321+joshnaganja@users.noreply.github.com>
2023-08-15 10:26:34 +0530

    #include<stdio.h> int main() {     int n,s=0,t,r,a=1,b,c;     scanf("%d",&n);     t=n;     while(n!=0)     {         r=n%10;         s=s+r;         n=n/10;     }     while(t!=0)     {         b=t%10;         a=a*b;         t=t/10;     }     if(s==a)     {         printf("Spy Number");     }     else     {         printf("Not Spy Number");     } }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,s=0,p=1,d;
+    int n,s=0,t,r,a=1,b,c;
     scanf("%d",&n);
-    for(;n>0;)
+    t=n;
+    while(n!=0)
     {
-        d=n%10;
-        s=s+d;
-        p=p*d;
+        r=n%10;
+        s=s+r;
         n=n/10;
     }
-    if(s==p)
+    while(t!=0)
+    {
+        b=t%10;
+        a=a*b;
+        t=t/10;
+    }
+    if(s==a)
     {
         printf("Spy Number");
     }
